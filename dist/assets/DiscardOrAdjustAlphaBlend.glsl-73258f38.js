e452673cb9e7a9c5b9cfc18823d344cfb4409a91
MohanRaj Balumuri <balumuriraj@gmail.com>
2023-02-21 13:47:44 -0800

    remove dist

--- dist/assets/DiscardOrAdjustAlphaBlend.glsl-73258f38.js
+++
@@ -1,3 +0,0 @@
-import{t as d}from"./AlphaCutoff-96178e0d.js";import{n as o}from"./ShaderBuilder-a7d0da4e.js";function f(r){r.fragment.code.add(o`
-    #define discardOrAdjustAlpha(color) { if (color.a < ${o.float(d)}) { discard; } }
-  `)}export{f as d};
